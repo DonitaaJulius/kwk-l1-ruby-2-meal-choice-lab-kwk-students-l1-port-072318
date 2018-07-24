bc236efbe9cca2f6a83649965939f8f44092e1a5
DonitaaJulius <donitaajulius@gmail.com>
2018-07-24 21:53:32 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,16 +1,16 @@
-puts "How many dresses do you have?"
-dresses = gets.strip.to_i
+# puts "How many dresses do you have?"
+# dresses = gets.strip.to_i
 
-puts "How many dresses do you want to buy in total?"
-closet = gets.strip.to_i
+# puts "How many dresses do you want to buy in total?"
+# closet = gets.strip.to_i
 
 
-while dresses <= closet
-  puts "You currently have #{dresses} in your closet. Now buy more!"
-  puts "How many dresses did you buy this shopping trip?"
-  purchase = gets.strip.to_i
-  puts "You have bought #{purchase} dresses!"
-  puts "Now you have #{dresses + purchase} in your closet"
-  dresses = dresses + purchase
-end 
+# while dresses <= closet
+#   puts "You currently have #{dresses} in your closet. Now buy more!"
+#   puts "How many dresses did you buy this shopping trip?"
+#   purchase = gets.strip.to_i
+#   puts "You have bought #{purchase} dresses!"
+#   puts "Now you have #{dresses + purchase} in your closet"
+#   dresses = dresses + purchase
+# end 
 
